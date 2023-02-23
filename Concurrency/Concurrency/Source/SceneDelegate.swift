fc32b741b604ff51d413f466c18b70348c85ef76
Zhilly <99257965+zhilly11@users.noreply.github.com>
2023-02-23 16:48:29 +0900

    chore: 프로젝트 초기 설정

---
+++ Concurrency/Concurrency/Source/SceneDelegate.swift
@@ -0,0 +1,15 @@
+//  Concurrency - SceneDelegate.swift
+//  Created by zhilly on 2023/02/23
+
+import UIKit
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
---
+++ Concurrency/Concurrency/Source/ViewController.swift
@@ -0,0 +1,11 @@
+//  Concurrency - ViewController.swift
+//  Created by zhilly on 2023/02/23
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
